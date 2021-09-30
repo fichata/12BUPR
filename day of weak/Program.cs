69ea9da4fd193a0e8e72f0f394661af842ad5d12
fichata <f.terziiski2017@pmg-pernik.com>
2021-09-30 13:23:58 +0300

    jhn

---
+++ day of weak/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day_of_weak
+{
+    class SumEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            {
+                int sum = 0;
+                for (int i=0; i < numbers.Length; i++)
+            }
+            
+        }
+    }
+}
